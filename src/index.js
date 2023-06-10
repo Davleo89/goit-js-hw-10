4e58368a45305d29128d76f38f32a5204de0fc6b
davleo <david.amadoral@gmail.com>
2023-06-10 18:15:40 -0500

    task

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
+import Notiflix from 'notiflix';
+
+const API_URL = `https://api.thecatapi.com/v1/breeds`;
+const API_KEY = 'live_rfV2x2ropS4pZ5RcmVUZYfglPKyBOu57OibpbNegtVBoRk8GypFOoEC6eJdhhhQw';
+
 const breedSelect = document.querySelector('.breed-select');
 const loader = document.querySelector('.loader');
 const msg_error = document.querySelector('.error');
 const catInfo = document.querySelector('.cat-info');
+const textError = msg_error.textContent;
 
 let storedBreeds = [];
 
-fetch(`https://api.thecatapi.com/v1/breeds`, {
+fetch(API_URL, {
   headers: {
-    'x-api-key': 'live_rfV2x2ropS4pZ5RcmVUZYfglPKyBOu57OibpbNegtVBoRk8GypFOoEC6eJdhhhQw',
+    'x-api-key': API_KEY,
   },
 })
   .then(response => {
@@ -36,20 +42,22 @@ fetch(`https://api.thecatapi.com/v1/breeds`, {
   .catch(function (error) {
     console.log(error);
     loader.style.display = 'none';
-    msg_error.style.display = 'block';
+    msg_error.style.display = 'none';
+    Notiflix.Notify.failure(textError);
   });
-
-function showBreedImage(index) {
-  const breed = storedBreeds[index];
-  catInfo.innerHTML = `
-    <img src="${breed.image.url}" id="breed_image" />
-    <p id="breed_json">${breed.temperament}</p>
-    <a id="wiki_link" href="${breed.wikipedia_url}">${breed.wikipedia_url}</a>
-  `;
-}
-
-breedSelect.addEventListener('change', function () {
-  const selectedIndex = breedSelect.value;
-  showBreedImage(selectedIndex);
-});
+  
+  function showBreedImage(index) {
+    const breed = storedBreeds[index];
+    catInfo.innerHTML = `
+      <img src="${breed.image.url}" class="breed_image" />
+      <div class="breed-details">
+      <p id="breed_json">${breed.temperament}</p>
+      <a id="wiki_link" href="${breed.wikipedia_url}">${breed.wikipedia_url}</a>
+      </div>`;
+  }
+  
+  breedSelect.addEventListener('change', function () {
+    const selectedIndex = breedSelect.value;
+    showBreedImage(selectedIndex);
+  });
 
